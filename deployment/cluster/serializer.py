b6a77f3c7b44f326862783414fd5578ac5a22c14
Mukeshjavvaji <70268956+Mukeshjavvaji@users.noreply.github.com>
2022-05-08 22:45:12 +0530

    Delete serializer.py

--- deployment/cluster/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import DataPoint
-
-class DataPointSerializers(serializers.ModelSerializer):
-    class meta:
-        model = DataPoint
-        fields = '__all__'
-
